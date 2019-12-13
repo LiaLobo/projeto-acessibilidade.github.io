0c5553fd26eeb791d82c38a54c13df0c6b52348b
LiaLobo <lia.lobo@gmail.com>
2019-12-12 22:27:06 -0300

    continua conteúdo página comece

--- src/pages/Start/Start.js
+++ src/pages/Start/Start.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import Tips from './components/Tips/Tips'
 import PrintAlt from '../../assets/Img/print-alt.PNG'
+import Cabeçalho from '../../assets/Img/print-cabeçalho.PNG'
 import Footer from '../../components/Footer/Footer'
 
 import './start.css'
@@ -25,12 +26,22 @@ const Start = () => (
                     alt='print da tela de desenvolvimento de site'
                 />
                 <Tips
-                    title='Adicionar o atributo alt nas imagens'
+                    title='Respeitar as tags de cabeçalho - <h1>, <h2>, <h3>, <h4>, <h5>, <h6>'
                     text='
-                        Esse atributo permite que o usuário com deficiência visual ou cognitiva entenda o que está sendo exibido na imagem,
-                        através dos leitores de tela.
+                        As tags de cabeçalho tem uma hierarquia, sendo <h1> a mais importante e <h6> com menos importância. É importante
+                        colocar essas tags na ordem para não haver uma quebra de hierarquia e possíveis problemas de SEO, além de usá-las em
+                        ordem crescente. Ajudam na experiência de usuários de tecnologia assistivas.Com elas conseguem navegar pelos títulos.
                     '
-                    source={PrintAlt}
+                    source={Cabeçalho}
+                    alt='print da tela de desenvolvimento de site'
+                />
+                <Tips
+                    title='Associar uma tag label aos elementos de formulário'
+                    text='
+                        A tag <label> é como um rótulo necessário para ajudar todos os leitores a entender o que precisa ser preeenchido nos inputs
+                        ou outros elementos do formulário
+                    '
+                    source={Cabeçalho}
                     alt='print da tela de desenvolvimento de site'
                 />
             </section>
